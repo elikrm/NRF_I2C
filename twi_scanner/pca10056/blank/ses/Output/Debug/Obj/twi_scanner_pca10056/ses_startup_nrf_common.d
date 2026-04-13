a27dd87d1735ca167f92f1d17e840d3f1e2986e2
Elnaz <elnazkarimpoor85@gmail.com>
2026-04-13 13:30:01 +1000

    starting with twi_scanner

---
+++ twi_scanner/pca10056/blank/ses/Output/Debug/Obj/twi_scanner_pca10056/ses_startup_nrf_common.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/twi_scanner_pca10056/ses_startup_nrf_common.o: \
+ C:\Elnaz\stateMachine_snakeGame\snake-firmware\homePractice\nRF52\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\ses_startup_nrf_common.s
